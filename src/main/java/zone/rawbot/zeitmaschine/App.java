4359709e51dc9135b0fae6fb6cf6a76e3ab0753e
Espen Jervidalo <espen@espen.ch>
2018-11-17 15:41:48 +0100

    Maven Project skeleton

---
+++ src/main/java/zone/rawbot/zeitmaschine/App.java
@@ -0,0 +1,13 @@
+package zone.rawbot.zeitmaschine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
