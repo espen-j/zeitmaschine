cbc3e42a911e84ec8e91a593d7ad7ac6d4f7dfe4
Espen Jervidalo <espen@espen.ch>
2018-12-08 22:37:44 +0100

    Read meta information from s3 image, rename artifact

--- prozessor/src/main/java/zone/rawbot/zeitmaschine/prozessor/rest/S3Notify.java
+++ prozessor/src/main/java/zone/rawbot/zeitmaschine/prozessor/rest/S3Notify.java
@@ -1,19 +1,27 @@
 package zone.rawbot.zeitmaschine.prozessor.rest;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Metadata;
+import com.jayway.jsonpath.JsonPath;
 import io.minio.MinioClient;
+import io.minio.ObjectStat;
 import io.minio.Result;
-import io.minio.errors.MinioException;
+import io.minio.errors.InvalidEndpointException;
+import io.minio.errors.InvalidPortException;
 import io.minio.messages.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import javax.annotation.PostConstruct;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * S3 notify endpoint.
@@ -21,27 +29,59 @@ import java.security.NoSuchAlgorithmException;
 @RestController
 public class S3Notify {
 
+    private final static Logger log = LoggerFactory.getLogger(S3Notify.class.getName());
+
+    public static final String ENDPOINT = "http://localhost:9000";
+    public static final String MINIO_ACCESS_KEY = "test";
+    public static final String MINIO_SECRET_KEY = "testtest";
+    public static final String BUCKET_NAME = "media";
+    private MinioClient minioClient;
+
+    @PostConstruct
+    private void init() throws InvalidPortException, InvalidEndpointException {
+        this.minioClient = new MinioClient(ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY);
+    }
+
     @PutMapping("api/webhook")
-    public ResponseEntity create(@RequestBody String body) {
+    public ResponseEntity create(@RequestBody String json) {
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("api/webhook")
-    public ResponseEntity update(@RequestBody String body) {
+    public ResponseEntity update(@RequestBody String json) {
+
+        List<String> keys = JsonPath.read(json, "$.Records[*].s3.object.key");
+
+        keys.stream()
+                .map(key -> getImage(key))
+                .filter(optional -> optional.isPresent())
+                .peek(url -> log.info(url.get()))
+                .findAny();
         return ResponseEntity.ok().build();
+    }
+
+    private  Optional<String> getImage(String key) {
+        try {
+            ObjectStat stat = minioClient.statObject(BUCKET_NAME, key);
+            String contentType = stat.contentType();
+            if (contentType.equals(MediaType.IMAGE_JPEG_VALUE)) {
+                InputStream inputStream = minioClient.getObject(BUCKET_NAME, key);
+                Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
+                metadata.getDirectories().forEach(directory -> log.info(directory.getName()));
+            }
+            return Optional.of(minioClient.getObjectUrl(BUCKET_NAME, key));
+
+        } catch (Exception e) {
+            log.error("Could not get image url for '{}'.", key, e);
+        }
+        return Optional.empty();
     }
 
     private void getImages() {
         try {
-            /* play.minio.io for test and development. */
-            MinioClient minioClient = new MinioClient("http://localhost:9000", "test",
-                    "testtest");
-
-            // Check whether 'my-bucketname' exist or not.
-            boolean found = minioClient.bucketExists("pics");
+            boolean found = minioClient.bucketExists(BUCKET_NAME);
             if (found) {
-                // List objects from 'my-bucketname'
-                Iterable<Result<Item>> myObjects = minioClient.listObjects("pics");
+                Iterable<Result<Item>> myObjects = minioClient.listObjects(BUCKET_NAME);
                 for (Result<Item> result : myObjects) {
                     Item item = result.get();
                     System.out.println(item.lastModified() + ", " + item.size() + ", " + item.objectName());
@@ -49,7 +89,7 @@ public class S3Notify {
             } else {
                 System.out.println("bucket name does not exist");
             }
-        } catch (MinioException | NoSuchAlgorithmException | IOException | InvalidKeyException | XmlPullParserException e) {
+        } catch (Exception e) {
             System.out.println("Error occurred: " + e);
         }
     }
