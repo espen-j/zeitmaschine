bb2947ea23f46b3f69c660640eacba6357eb62c8
Espen Jervidalo <espen@espen.ch>
2018-12-08 22:36:04 +0100

    Use static index file, asset resolving, title from webpack

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,12 +2,15 @@ module.exports = {
     pages: {
         index: {
             // entry for the page
-            entry: __dirname + '/src/main/resources/vue/src/main.ts'
+            entry: __dirname + '/src/main/resources/vue/src/main.ts',
+            // the source template
+            template: __dirname + '/src/main/resources/vue/public/index.html',
+            // when using title option,
+            // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'zeitmaschine'
         }
     },
     // Change build paths to make them Maven compatible
     // see https://cli.vuejs.org/config/
-    outputDir: __dirname + '/target/dist',
-    assetsDir: 'static'
-
+    outputDir: __dirname + '/target/dist'
 }
