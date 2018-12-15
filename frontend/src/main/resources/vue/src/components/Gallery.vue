78e5783a75aa237991240a4f8b620d19293bfc79
Espen Jervidalo <espen@espen.ch>
2018-12-15 19:11:38 +0100

    Minimal styling for images

--- frontend/src/main/resources/vue/src/components/Gallery.vue
+++ frontend/src/main/resources/vue/src/components/Gallery.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <div v-for="image in images">
-      <img :src="image.thumbnail" width="120">
+    <div class="container">
+        <div class="cell" v-for="image in images">
+            <img :src="image.thumbnail">
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -26,19 +26,23 @@ export default class Gallery extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style >
+  .container {
+    margin: 0 auto;
+    max-width: 1200px;
+    padding: 0 1rem;
+  }
+
+  .cell {
+      width: 150px;
+      height: 150px;
+      float: left;
+  }
+
+  .cell img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+  }
+
 </style>
