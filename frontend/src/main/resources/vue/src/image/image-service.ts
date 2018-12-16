4d6c6d4f3fda21d126b41c336001c46142e9bffe
Espen Jervidalo <espen@espen.ch>
2018-12-16 14:34:34 +0100

    Point image service to elastic endpoint, transform ES response to iamge array

--- frontend/src/main/resources/vue/src/image/image-service.ts
+++ frontend/src/main/resources/vue/src/image/image-service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
-
 import {Image} from './image';
+
+const ELASTIC_ENDPOINT: string = 'http://localhost:9200/zeitmaschine/image/_search';
 
 class ImageService {
     constructor() {
@@ -8,9 +9,23 @@ class ImageService {
     }
 
     public getImages() {
-        return axios.get<Image[]>(`/image`);
+        return axios.get<Image[]>(ELASTIC_ENDPOINT, {
+            transformResponse: data => {
+                return this.transform(data);
+            }
+        });
+    }
+
+    private transform(response: string): Image[] {
+        return JSON.parse(response).hits.hits.map((hit: any) => {
+            return {
+                name: hit._source.name,
+                thumbnail: hit._source.thumbnail
+            };
+        });
     }
 }
+
 
 // Export a singleton instance in the global namespace
 export const imageService = new ImageService();
