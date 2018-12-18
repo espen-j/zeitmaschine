cb39540c601a38767ea1898e7ee12e218b02b332
Espen Jervidalo <espen@espen.ch>
2018-12-18 20:52:13 +0100

    Fix naming of scaling function

--- prozessor/src/main/java/zone/rawbot/zeitmaschine/prozessor/s3/S3Repository.java
+++ prozessor/src/main/java/zone/rawbot/zeitmaschine/prozessor/s3/S3Repository.java
@@ -90,28 +90,28 @@ public class S3Repository {
         return Flux.empty();
     }
 
-    public BufferedImage scaleImage(BufferedImage img, int width, int height,
+    public BufferedImage scaleImage(BufferedImage source, int width, int height,
                                     Color background) {
-        int imgWidth = img.getWidth();
-        int imgHeight = img.getHeight();
+        int imgWidth = source.getWidth();
+        int imgHeight = source.getHeight();
         if (imgWidth*height < imgHeight*width) {
             width = imgWidth*height/imgHeight;
         } else {
             height = imgHeight*width/imgWidth;
         }
-        BufferedImage newImage = new BufferedImage(width, height,
+        BufferedImage scaled = new BufferedImage(width, height,
                 BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = newImage.createGraphics();
+        Graphics2D g = scaled.createGraphics();
         try {
             g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
                     RenderingHints.VALUE_INTERPOLATION_BICUBIC);
             g.setBackground(background);
             g.clearRect(0, 0, width, height);
-            g.drawImage(img, 0, 0, width, height, null);
+            g.drawImage(source, 0, 0, width, height, null);
         } finally {
             g.dispose();
         }
-        return newImage;
+        return scaled;
     }
 
 }
