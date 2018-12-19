bbcc97137a64d8e5dbbf73dada9a87bf7e931045
Espen Jervidalo <espen@espen.ch>
2018-12-19 19:17:24 +0100

    Add paging support for image-service

--- frontend/src/main/resources/vue/src/image/image-service.ts
+++ frontend/src/main/resources/vue/src/image/image-service.ts
@@ -2,22 +2,29 @@ import axios from 'axios';
 import {Image} from './image';
 
 const ELASTIC_ENDPOINT: string = 'http://localhost:9200/zeitmaschine/image/_search';
+const PAGING_SIZE: number = 64;
 
 class ImageService {
+
     constructor() {
         console.log('creating new instance of image-service');
     }
 
-    public getImages() {
-        return axios.get<Image[]>(ELASTIC_ENDPOINT, {
+    public getImages(from: number = 0) {
+
+        return axios.post<Image[]>(ELASTIC_ENDPOINT, {
+                from,
+                size : PAGING_SIZE
+            }, {
             transformResponse: data => {
-                return this.transform(data);
+                const json = JSON.parse(data);
+                return (json.hits) ? this.transform(json.hits) : [];
             }
         });
     }
 
-    private transform(response: string): Image[] {
-        return JSON.parse(response).hits.hits.map((hit: any) => {
+    private transform(json: any): Image[] {
+        return json.hits.map((hit: any) => {
             return {
                 name: hit._source.name,
                 thumbnail: hit._source.thumbnail
