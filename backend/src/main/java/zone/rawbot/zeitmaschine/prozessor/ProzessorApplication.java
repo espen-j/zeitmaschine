422e03922c9493ed5d5c113c5a26c93b926b8cfc
Espen Jervidalo <espen@espen.ch>
2019-02-12 19:15:15 +0100

    Load index file from frontend project

--- backend/src/main/java/zone/rawbot/zeitmaschine/prozessor/ProzessorApplication.java
+++ backend/src/main/java/zone/rawbot/zeitmaschine/prozessor/ProzessorApplication.java
@@ -1,12 +1,34 @@
 package zone.rawbot.zeitmaschine.prozessor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.TEXT_HTML;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @SpringBootApplication
 public class ProzessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProzessorApplication.class, args);
-	}
+    // https://github.com/spring-projects/spring-boot/issues/9785
+    @Value("classpath:/static/index.html")
+    private Resource indexHtml;
+
+    @Bean
+    public RouterFunction<ServerResponse> routes() {
+        // https://github.com/spring-projects/spring-boot/issues/9785
+        return route(
+                GET("/").and(accept(TEXT_HTML)), request -> ServerResponse.ok().syncBody(indexHtml))
+                .andRoute(GET("/login").and(accept(TEXT_HTML)), request -> ServerResponse.ok().syncBody(indexHtml));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProzessorApplication.class, args);
+    }
 }
