598e98e9c4e4d9151b580b64b61d649f7ff479bd
Espen Jervidalo <espen@espen.ch>
2019-08-27 19:14:46 +0200

    Revert "Initial swiping with hammer js"
    This reverts commit bfb13f2e14ad155c83f81c227ed9cb31ce0800ae.

--- frontend/src/main/resources/vue/src/components/Slider.vue
+++ frontend/src/main/resources/vue/src/components/Slider.vue
@@ -1,33 +1,16 @@
 <template>
     <div class="slider">
         <span class="close-button" v-on:click="close()">X</span>
-        <img v-pan="onPan" ref="image" :src="src">
+        <img :src="src">
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Prop, Vue} from 'vue-property-decorator';
-    import 'hammerjs';
     import {Image} from '../image/image';
     import {imageService} from '../image/image-service';
 
-    @Component({
-        directives: {
-            // https://lisilinhart.info/posts/touch-interaction-vue
-            pan: (el, binding) => {
-                if (binding.value instanceof Function) {
-                    // create a manager for that element
-                    let mc = new Hammer.Manager(el);
-                    // create a recognizer
-                    let pan = new Hammer.Pan({direction: Hammer.DIRECTION_HORIZONTAL});
-                    // add the recognizer
-                    mc.add(pan);
-                    // subscribe to events
-                    mc.on('pan', binding.value);
-                }
-            }
-        }
-    })
+    @Component
     export default class Slider extends Vue {
 
         @Prop()
@@ -43,19 +26,6 @@
                 .then(blob => URL.createObjectURL(blob))
                 .then(src => this.src = src)
                 .catch(e => console.log(e));
-        }
-
-        onPan(event: any) {
-            console.log(event);
-            const deltaX = event.deltaX; // moved distance on x-axis
-            const deltaY = event.deltaY; // moved distance on y-axis
-            const isFinal = event.isFinal; // pan released
-            const direction = event.direction; // 0 = none, 2 = left, 4 = right, 8 = up, 16 = down,
-            event.target.style.left = event.deltaX + 'px';
-            if (isFinal) {
-                event.target.style.left = 0 + 'px';
-            }
-            console.log(deltaX, deltaY, isFinal, direction)
         }
     }
 </script>
@@ -78,12 +48,10 @@
             right: 30px;
             top: 30px;
             cursor: pointer;
-            z-index: 100;
         }
 
         img {
             object-fit: scale-down;
-            position: relative;
         }
     }
 </style>
