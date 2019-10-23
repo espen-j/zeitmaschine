d80bb6593b2a17d3ab43101c15786a569a0ec704
Espen Jervidalo <espen@espen.ch>
2019-10-23 13:36:10 +0800

    Fix lost EXIF orientation flag in test case. Worse performance

--- backend/src/test/java/io/zeitmaschine/image/ScalerTest.java
+++ backend/src/test/java/io/zeitmaschine/image/ScalerTest.java
@@ -1,5 +1,12 @@
 package io.zeitmaschine.image;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.twelvemonkeys.image.AffineTransformOp;
 import com.twelvemonkeys.image.ResampleOp;
 import org.imgscalr.Scalr;
 import org.junit.Ignore;
@@ -10,6 +17,7 @@ import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 import java.io.IOException;
@@ -25,20 +33,22 @@ class ScalerTest {
     private BufferedImage input;
     private String originalImage;
 
-    private int width = Dimension.THUMBNAIL.getSize();
-    private int height = Dimension.THUMBNAIL.getSize();
+    private int width = Dimension.SMALL.getSize();
+    private int height = Dimension.SMALL.getSize();
 
     private String[] images = {"IMG_20161208_024708.jpg", "IMG_20180614_214734.jpg"};
 
     @BeforeEach
-    void setUp() throws IOException {
-        this.originalImage = "IMG_20161208_024708.jpg";
+    void setUp() throws Exception {
+        this.originalImage = "IMG_20181001_185137.jpg";
         InputStream stream = ClassLoader.getSystemResourceAsStream("images/" + originalImage);
 
         this.input = ImageIO.read(stream);
 
         int imgWidth = input.getWidth();
         int imgHeight = input.getHeight();
+
+
 
         if (imgWidth * height < imgHeight * width) {
             width = imgWidth * height / imgHeight;
@@ -99,7 +109,7 @@ class ScalerTest {
     @Test
     void progressive() throws IOException {
 
-        String oName = "IMG_20180614_214734.jpg";
+        String oName = "IMG_20181001_185137.jpg";
 
         Path output = outputDirectory.resolve("progressive_" + oName);
         if (!Files.exists(output)) {
@@ -120,7 +130,7 @@ class ScalerTest {
     @Test
     void scalrProgressive() throws IOException {
 
-        String oName = "IMG_20180614_214734.jpg";
+        String oName = "IMG_20181001_185137.jpg";
 
         Path output = outputDirectory.resolve("scalr_progressive_" + oName);
         if (!Files.exists(output)) {
@@ -149,12 +159,57 @@ class ScalerTest {
 
         long before = System.currentTimeMillis();
 
-        BufferedImage image = Scaler.scale(input, Dimension.THUMBNAIL);
+        BufferedImage image = Scaler.scale(input, Dimension.SMALL);
 
         long after = System.currentTimeMillis();
 
         long time = after - before;
         System.out.println("Laszlo took ms: " + time);
+
+        ImageIO.write(image, "jpg", output.toFile());
+    }
+
+    @Test
+    void laszloOriented() throws IOException, ImageProcessingException, MetadataException {
+
+        String oName = "IMG_20181001_185137.jpg";
+
+        Path output = outputDirectory.resolve("laszloOriented_" + oName);
+        if (!Files.exists(output)) {
+            output = Files.createFile(output);
+        }
+
+        long before = System.currentTimeMillis();
+
+
+        Metadata metadata = ImageMetadataReader.readMetadata(ClassLoader.getSystemResourceAsStream("images/" + originalImage));
+
+        // original an thumbnail?
+        Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+
+        int imgHeight = input.getHeight();
+        int imgWidth = input.getWidth();
+        int orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+        if (orientation > 1) {
+            AffineTransform operation = getExifTransformation(imgHeight, imgWidth, orientation);
+            this.input = transformImage(input, operation);
+            imgWidth = input.getWidth();
+            imgHeight = input.getHeight();
+        }
+
+
+        if (imgWidth * height < imgHeight * width) {
+            width = imgWidth * height / imgHeight;
+        } else {
+            height = imgHeight * width / imgWidth;
+        }
+
+        BufferedImage image = Scaler.scale(input, Dimension.SMALL);
+
+        long after = System.currentTimeMillis();
+
+        long time = after - before;
+        System.out.println("LaszloOriented took ms: " + time);
 
         ImageIO.write(image, "jpg", output.toFile());
     }
@@ -208,5 +263,61 @@ class ScalerTest {
         graphics2D.drawImage(imageToScale, 0, 0, dWidth, dHeight, null);
         graphics2D.dispose();
         return scaledImage;
+    }
+
+    // Look at http://chunter.tistory.com/143 for information
+    public static AffineTransform getExifTransformation(int height, int width, int orientation) {
+
+        AffineTransform t = new AffineTransform();
+
+        switch (orientation) {
+            case 1:
+                break;
+            case 2: // Flip X
+                t.scale(-1.0, 1.0);
+                t.translate(-width, 0);
+                break;
+            case 3: // PI rotation
+                t.translate(width, height);
+                t.rotate(Math.PI);
+                break;
+            case 4: // Flip Y
+                t.scale(1.0, -1.0);
+                t.translate(0, -height);
+                break;
+            case 5: // - PI/2 and Flip X
+                t.rotate(-Math.PI / 2);
+                t.scale(-1.0, 1.0);
+                break;
+            case 6: // -PI/2 and -width
+                t.translate(height, 0);
+                t.rotate(Math.PI / 2);
+                break;
+            case 7: // PI/2 and Flip
+                t.scale(-1.0, 1.0);
+                t.translate(-height, 0);
+                t.translate(0, width);
+                t.rotate(  3 * Math.PI / 2);
+                break;
+            case 8: // PI / 2
+                t.translate(0, width);
+                t.rotate(  3 * Math.PI / 2);
+                break;
+        }
+
+        return t;
+    }
+
+    public static BufferedImage transformImage(BufferedImage image, AffineTransform transform) {
+
+        AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BICUBIC);
+
+        // Create an instance of the resulting image, with the same width, height and image type than the referenced one
+        // TODO that's not true, need to switch the widthor height based e.g. for 90 degrees rotation (case 6)
+        BufferedImage destinationImage = new BufferedImage( image.getHeight(), image.getWidth(), image.getType() );
+        op.filter(image, destinationImage);
+
+        // Set the created image as new buffered image
+        return destinationImage;
     }
 }
