898e4beaa7fe68461ceef16a18b340b7ca80802e
Espen Jervidalo <espen@espen.ch>
2020-10-26 08:31:21 +0100

    Add failing test for return status

--- backend/src/test/java/io/zeitmaschine/image/ImagingIntegrationTest.java
+++ backend/src/test/java/io/zeitmaschine/image/ImagingIntegrationTest.java
@@ -129,4 +129,32 @@ public class ImagingIntegrationTest {
                     }
                 });
     }
+
+    // FIXME: null pointer, instead http return status!
+    @Test
+    @WithMockUser
+    void nonExistingObject() {
+        webTestClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("image/")
+                        .path(String.valueOf(Dimension.SMALL))
+                        .queryParam("name", "DOES_NOT_EXIST.jpg")
+                        .build())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE)
+                .accept(MediaType.IMAGE_JPEG)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.IMAGE_JPEG)
+                .expectBody(byte[].class)
+                .consumeWith(response -> {
+                    byte[] responseBody = response.getResponseBody();
+                    try {
+                        BufferedImage img = ImageIO.read(new ByteArrayInputStream(responseBody));
+                        assertThat(img.getWidth(), CoreMatchers.is(Dimension.SMALL.getSize()));
+                    } catch (IOException e) {
+                        fail(e.getMessage());
+                    }
+                });
+    }
+
 }
