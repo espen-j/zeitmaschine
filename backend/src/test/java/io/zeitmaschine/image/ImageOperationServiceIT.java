82b04c5f8ebf36949fd7c78607f140af5f56ff78
Espen Jervidalo <espen@espen.ch>
2020-11-16 19:17:35 +0100

    Introduce testcontainers for ImageOperationServiceIT

--- backend/src/test/java/io/zeitmaschine/image/ImageOperationServiceIT.java
+++ backend/src/test/java/io/zeitmaschine/image/ImageOperationServiceIT.java
@@ -1,34 +1,51 @@
 package io.zeitmaschine.image;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.stream.Stream;
+
+import javax.imageio.ImageIO;
+
 import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.stream.Stream;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("integration-test")
+@Testcontainers
 public class ImageOperationServiceIT {
     private static final String[] images = {"IMG_20161208_024708.jpg", "IMG_20180614_214734.jpg", "IMG_20181001_185137.jpg"};
 
-    @Autowired
+    private static final int EXPOSED_PORT = 8088;
+
+    // will be shared between test methods
+    @Container
+    private static GenericContainer container = new GenericContainer(DockerImageName.parse("h2non/imaginary:1.2.4"))
+                .withEnv("PORT", String.valueOf(EXPOSED_PORT))
+                .withExposedPorts(EXPOSED_PORT);
+
     private ImageOperationService operationService;
+
+    @BeforeEach
+    void setUp() {
+        ImageOperationConfig config = new ImageOperationConfig();
+        String host = "http://" + container.getHost() + ":" + container.getFirstMappedPort();
+        config.setHost(host);
+        this.operationService = new ImageOperationService(config);
+    }
 
     @ParameterizedTest
     @ArgumentsSource(TestImagesProvider.class)
