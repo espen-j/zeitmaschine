a786433437b539856ea56d593cd97de5f57b6249
Espen Jervidalo <espen@espen.ch>
2020-11-16 19:29:49 +0100

    Merge ImageOperationServiceIT and ImaginaryIT into one test class

--- backend/src/test/java/io/zeitmaschine/image/ImageOperationServiceIT.java
+++ backend/src/test/java/io/zeitmaschine/image/ImageOperationServiceIT.java
@@ -3,6 +3,7 @@ package io.zeitmaschine.image;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.stream.Stream;
@@ -17,9 +18,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -27,6 +35,9 @@ import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 public class ImageOperationServiceIT {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ImageOperationServiceIT.class.getName());
+
     private static final String[] images = {"IMG_20161208_024708.jpg", "IMG_20180614_214734.jpg", "IMG_20181001_185137.jpg"};
 
     private static final int EXPOSED_PORT = 8088;
@@ -38,11 +49,12 @@ public class ImageOperationServiceIT {
                 .withExposedPorts(EXPOSED_PORT);
 
     private ImageOperationService operationService;
+    private ImageOperationConfig config;
 
     @BeforeEach
     void setUp() {
-        ImageOperationConfig config = new ImageOperationConfig();
         String host = "http://" + container.getHost() + ":" + container.getFirstMappedPort();
+        this.config = new ImageOperationConfig();
         config.setHost(host);
         this.operationService = new ImageOperationService(config);
     }
@@ -73,6 +85,40 @@ public class ImageOperationServiceIT {
     }
 
 
+    /*
+       Naked Imaginary API Call.
+     */
+    @ParameterizedTest
+    @ArgumentsSource(TestImagesProvider.class)
+    void imaginary(Resource image) throws IOException {
+        // https://www.baeldung.com/spring-5-webclient
+        // https://www.baeldung.com/webflux-webclient-parameters
+        WebClient webClient = WebClient
+                .builder()
+                .baseUrl(config.getHost())
+                .filter(logRequest())
+                .build();
+
+        ByteArrayInputStream bImg = webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("resize")
+                        .queryParam("width", 150)
+                        .build())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE)
+                .body(BodyInserters.fromResource(image))
+                .accept(MediaType.IMAGE_JPEG)
+                .retrieve()
+                .bodyToMono(byte[].class)
+                .map(ByteArrayInputStream::new)
+                .block();
+
+        BufferedImage img = ImageIO.read(bImg);
+
+        assertThat(img.getWidth(), CoreMatchers.is(150));
+
+    }
+
+
     private static class TestImagesProvider implements ArgumentsProvider {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
@@ -80,5 +126,14 @@ public class ImageOperationServiceIT {
                     .map(image -> new ClassPathResource("images/" + image))
                     .map(Arguments::of);
         }
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return (clientRequest, next) -> {
+            LOG.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+            clientRequest.headers()
+                    .forEach((name, values) -> values.forEach(value -> LOG.info("{}={}", name, value)));
+            return next.exchange(clientRequest);
+        };
     }
 }
